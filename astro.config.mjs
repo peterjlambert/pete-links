c025a3706d31f20ac55ab50dc87585a536b82f58
Pete Lambert <pete@petelambert.com>
2022-12-17 16:50:34 +0000

    Adds order back in temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,6 @@ export default defineConfig({
             preview_path: "{{ slug }}",
             layout: "../../layouts/Link.astro",
             sortable_fields: ["order", "title"],
-            widget: "list",
             fields: [
               { name: "title", widget: "string", label: "Title" },
               { name: "url", widget: "string", label: "URL" },
@@ -35,6 +34,24 @@ export default defineConfig({
                 options: [
                   { label: "Link", value: "link" },
                   { label: "Player", value: "player" },
+                ],
+              },
+              {
+                name: "sortOrder",
+                widget: "number",
+                label: "Sort Order",
+                default: "1",
+                options: [
+                  { label: "1", value: "1" },
+                  { label: "2", value: "2" },
+                  { label: "3", value: "3" },
+                  { label: "4", value: "4" },
+                  { label: "5", value: "5" },
+                  { label: "6", value: "6" },
+                  { label: "7", value: "7" },
+                  { label: "8", value: "8" },
+                  { label: "9", value: "9" },
+                  { label: "10", value: "10" },
                 ],
               },
             ],
