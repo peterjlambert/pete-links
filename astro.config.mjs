ce442dd1176c65ec0d39093c5c5fa1a16bf40cf6
Pete Lambert <pete@petelambert.com>
2022-12-27 18:35:46 +0000

    Half did and then half undid the changes to have a custom logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
+  integrations: [image()]
 });
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,7 +1,7 @@
 ---
 title: Pete Lambert Music
 description: Links & Stuff
-logo: /assets/innersense-logo.svg
+logo: /assets/plm.svg
 logoDescription: Innersense Aesthetics
 accentOne: "#B9594D"
 layout: ../layouts/Page.astro
